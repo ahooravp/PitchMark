ba0859017e2e43e6ee562a4efd552242af4769c3
ahoora <ahooravhp@gmail.com>
2026-04-21 21:02:38 +0330

    making the hero section and startupcard

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,10 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import localFont from "next/font/local";
 import Navbar from "@/components/Navbar";
-import { Geist } from "next/font/google";
 import { cn } from "@/lib/utils";
 
-const geist = Geist({subsets:['latin'],variable:'--font-sans'});
 
 const workSans = localFont({
   src: [
@@ -69,7 +67,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={cn("font-sans", geist.variable)}>
+    <html lang="en" className={cn("font-sans", workSans.variable)}>
       <Navbar />
       <body>{children}</body>
     </html>
