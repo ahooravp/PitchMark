880a2503eb00a3ecb442fa5510ee81fca140ee36
ahoora <ahooravhp@gmail.com>
2026-04-25 01:26:36 +0330

    updating queries to add startup details page

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,18 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     dangerouslyAllowSVG: true,
     remotePatterns: [{
       protocol: "https",
       hostname: "*"
     }]
+  },
+  // Use boolean, not string
+  cacheComponents: true,   // Enables Partial Prerendering (recommended)
+  devIndicators: {
+    appIsrStatus: true,
+    buildActivity: true,
+    buildActivityPosition: "bottom-right"
   }
 };
 
--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,8 +1,7 @@
 import { defineQuery } from "next-sanity";
 
-
-export const STARTUPS_QUERY =   
-  defineQuery(`*[_type == "startup" && defined(slug.current) && !defined($search) || category match $search || author -> name match $search || title match $search] | order(_createdAt desc) {
+export const STARTUPS_QUERY =
+  defineQuery(`*[_type == "startup" && defined(slug.current) && (!defined($search) || category match $search || author -> name match $search || title match $search)] | order(_createdAt desc) {
     _id,
     title,
     slug,
@@ -14,4 +13,20 @@ export const STARTUPS_QUERY =
     description,
     category,
     image
-  }`)
+  }`);
+
+export const STARTUP_BY_ID_QUERY =
+  defineQuery(`*[_type == "startup" && _id == $id][0] {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+      _id, name, username, image, bio    
+    },
+    views,
+    description,
+    category,
+    image,
+    pitch
+  }`);
